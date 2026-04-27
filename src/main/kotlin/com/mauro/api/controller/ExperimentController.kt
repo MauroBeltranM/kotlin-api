5800e02f9ed6b55904de77857198a0bc95a49162
Mauro Beltrán <maurobeltranmusic@gmail.com>
2026-04-27 16:31:29 +0200

    chore: remove unused MediaType import

--- src/main/kotlin/com/mauro/api/controller/ExperimentController.kt
+++ src/main/kotlin/com/mauro/api/controller/ExperimentController.kt
@@ -4,7 +4,6 @@ import com.mauro.api.model.*
 import com.mauro.api.service.ExperimentService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @RestController
